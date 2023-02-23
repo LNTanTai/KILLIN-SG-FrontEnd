335c4cb62bf78ea5d3e36836288a9e0e9419c68b
LNTanTai <Tailainguyentan@gmail.com>
2023-02-23 15:07:22 +0700

    Update Footer in HomePage

--- killin-fe/src/pages/HomePage/index.jsx
+++ killin-fe/src/pages/HomePage/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { Navbar, SizeBar } from "../../services/constants/componentConstants";
+import { Footer, Navbar, SizeBar } from "../../services/constants/componentConstants";
 import { Box } from "@mui/system";
 import { CssBaseline, Toolbar } from "@mui/material";
 
 const HomePage = () => {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex", flexDirection: 'column' }}>
       <CssBaseline />
       <Navbar />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-      <Toolbar />
-      <h1>ok</h1>
+        <Toolbar />
+        <h1>ok</h1>
       </Box>
+      <Footer/>
     </Box>
   );
 };
--- killin-fe/src/services/constants/componentConstants.js
+++ killin-fe/src/services/constants/componentConstants.js
@@ -1,3 +1,4 @@
 export {default as Navbar} from "../../components/Navbar"
 export {default as SizeBar} from "../../components/Sizebar"
+export {default as Footer} from "../../components/Footer"
 export {default as LoadingBackdrop} from "../../components/Loading"
